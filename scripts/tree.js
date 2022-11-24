e8eb1df0823c010bb76f5691b4c988fa650d734e
aholla <cc211034@fhstp.ac.at>
2022-11-24 13:32:33 +0100

    tree done

--- scripts/tree.js
+++ scripts/tree.js
@@ -1,1 +1,20 @@
-console.log('Hello I am a tree')
+const height = process.argv[2] ||10;
+const widestLayer = height - 1
+const maxLeaves = createLeaves(widestLayer)
+
+function createLeaves(layer){
+    return layer * 2 - 1
+}
+
+function createAir(layer){
+    return (maxLeaves - createLeaves(layer)) / 2
+}
+
+
+for(let i = 1; i<=widestLayer; i++){
+    console.log(' '.repeat(createAir(i))+'#'.repeat(createLeaves(i)))
+}
+console.log(' '.repeat(createAir(1))+'#'.repeat(createLeaves(1)))
+
+
+
